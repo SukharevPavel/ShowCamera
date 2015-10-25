acff89347c72079f9e34f1c3e3b10261e4a90ce8
Pavel <suharev_p@mail.ru>
2015-10-25 19:08:21 +0300

    Make app working for old Camera Api.

--- app/src/main/java/ru/suharev/showcamera/ui/CameraActivityFragment.java
+++ app/src/main/java/ru/suharev/showcamera/ui/CameraActivityFragment.java
@@ -1,14 +1,13 @@
 package ru.suharev.showcamera.ui;
 
-import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import ru.suharev.showcamera.R;
 import ru.suharev.showcamera.utils.CameraData;
@@ -16,12 +15,24 @@ import ru.suharev.showcamera.utils.CameraData;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class CameraActivityFragment extends Fragment {
+public class CameraActivityFragment extends ListFragment {
+
+    public static final int CAMERA_DISABLED = -1;
+    public static final String EXTRA_CAMERA_NUMBER = "camera_number";
+
 
     private CameraData mCameraData;
     private SurfaceView mSurfaceView;
+    private ArrayAdapter<String> mAdapter;
+    private int mCurrentCamera = CAMERA_DISABLED;
 
     public CameraActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setRetainInstance(true);
     }
 
     @Override
@@ -32,6 +43,52 @@ public class CameraActivityFragment extends Fragment {
         mSurfaceView = (SurfaceView) v.findViewById(R.id.video_view);
         return v;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey(EXTRA_CAMERA_NUMBER)) {
+            mCurrentCamera = savedInstanceState.getInt(EXTRA_CAMERA_NUMBER);
+            if (mCurrentCamera != CAMERA_DISABLED) {
+                mCameraData.showVideo(mCurrentCamera, mSurfaceView);
+            }
+        }
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        mAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_list_item_1,
+                mCameraData.getCameraIds());
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mCurrentCamera != position) {
+                    mCurrentCamera = position;
+                    mCameraData.showVideo(position, mSurfaceView);
+                } else {
+                    mCurrentCamera = CAMERA_DISABLED;
+                    mCameraData.hideVideo();
+                }
+            }
+        });
+        setListAdapter(mAdapter);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(EXTRA_CAMERA_NUMBER, mCurrentCamera);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if (mCurrentCamera != CAMERA_DISABLED) mCameraData.hideVideo();
+    }
+
+
 
 
 }
--- app/src/main/java/ru/suharev/showcamera/utils/CameraData.java
+++ app/src/main/java/ru/suharev/showcamera/utils/CameraData.java
@@ -4,14 +4,19 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 import ru.suharev.showcamera.R;
@@ -22,15 +27,19 @@ import ru.suharev.showcamera.R;
 public class CameraData {
 
     private List<String> mCameraIds;
+    private List<String> mCameraSugarIds;
     private Context mContext;
     private CameraManager mCameraManager;
+    private CameraDevice mCameraDevice;
+
+    private Camera mCamera;
 
     public CameraData(Context ctx) {
         mContext = ctx;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             initCameraLollipop();
         else
-            initCamera();
+            initCameraOld();
         fixCameraIds();
     }
 
@@ -46,7 +55,8 @@ public class CameraData {
         }
     }
 
-    private void initCamera() {
+    private void initCameraOld() {
+        mCameraIds = new ArrayList<>();
         int numbers = Camera.getNumberOfCameras();
         for (int i = 0; i < numbers; i++) {
             mCameraIds.add(String.valueOf(i));
@@ -54,17 +64,104 @@ public class CameraData {
     }
     
     private void fixCameraIds(){
-        for (int i = 0; i< mCameraIds.size(); i++) {
+        mCameraSugarIds = new ArrayList<>();
+        for (String id : mCameraIds) {
             String newId = mContext.getResources().
-                    getString(R.string.camera_sugar).concat(mCameraIds.get(i));
-                    mCameraIds.add(i,newId);
+                    getString(R.string.camera_sugar).concat(id);
+            mCameraSugarIds.add(newId);
         }
     }
 
 
-    private List<String> getCameraIds(){
-        return mCameraIds;
+    public List<String> getCameraIds() {
+        return mCameraSugarIds;
     }
 
+    public void showVideo(int cameraId, SurfaceView view) {
+        SurfaceHolder holder = view.getHolder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            showVideoLollipop(cameraId, holder);
+        else
+            showVideoOld(cameraId, holder);
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void showVideoLollipop(int cameraId, SurfaceHolder holder) {
+        showVideoOld(cameraId, holder);
+        try {
+            mCameraManager.openCamera(mCameraIds.get(cameraId), new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(CameraDevice camera) {
+                    mCameraDevice = camera;
+                }
+
+                @Override
+                public void onDisconnected(CameraDevice camera) {
+
+                }
+
+                @Override
+                public void onError(CameraDevice camera, int error) {
+                    Toast.makeText(mContext,
+                            mContext.getResources().getString(R.string.toast_error_camera),
+                            Toast.LENGTH_LONG).show();
+                }
+            }, null);
+            List<Surface> surface = Collections.singletonList(holder.getSurface());
+            mCameraDevice.createCaptureSession(surface, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showVideoOld(int cameraId, SurfaceHolder holder) {
+        hideVideo();
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e) {
+            Toast.makeText(mContext, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Toast.makeText(mContext,
+                    mContext.getResources().getString(R.string.toast_error_camera),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void hideVideo() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            hideVideoLollipop();
+        else
+            hideVideoOld();
+    }
+
+    private void hideVideoOld() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+
+        }
+    }
+
+    private void hideVideoLollipop() {
+        hideVideoOld();
+    }
 
 }
